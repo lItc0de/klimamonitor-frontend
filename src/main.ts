0d3938ca6d528f3ca4e88552f41da3d1b277c182
litc0de <litc0de@posteo.org>
2023-07-03 22:32:13 +0200

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,44 @@
+import './style.css'
+import videoSrc from '../ressources/video.mov?url';
+
+const MAX_DISTANCE = 3500;
+const MIN_DISTANCE = 3;
+const TOTAL_DISTANCE = MAX_DISTANCE - MIN_DISTANCE;
+
+document.querySelector<HTMLDivElement>('#video-container')!.innerHTML = `
+<video
+  id="video"
+  src="${videoSrc}"
+  controls="false"
+  crossorigin="anonymous"
+  width="100%"
+  height="100%"
+  muted="muted"
+/>
+`;
+
+
+const video = document.getElementById('video') as HTMLVideoElement;
+
+video.addEventListener('loadeddata', () => {
+  const duration = video.duration;
+  console.log('duration', duration);
+
+  video.controls = false;
+
+
+  const setVideoTime = (distance: number): void => {
+    const rawTime = Number((distance * duration / TOTAL_DISTANCE).toFixed(6));
+    const time = Math.max(0, Math.min(duration, rawTime))
+    console.log(time);
+
+    video.currentTime = time;
+  }
+
+  let distance = 3;
+
+  setInterval(() => {
+    setVideoTime(distance)
+    distance += 50;
+  }, 500);
+});
