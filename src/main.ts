1916d8c88a54fd7803fbcf0dd94e5f2795520fc1
litc0de <litc0de@posteo.org>
2023-07-03 22:33:11 +0200

    format

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './style.css'
-import videoSrc from '../ressources/video.mov?url';
+import "./style.css";
+import videoSrc from "../ressources/video.mov?url";
 
 const MAX_DISTANCE = 3500;
 const MIN_DISTANCE = 3;
 const TOTAL_DISTANCE = MAX_DISTANCE - MIN_DISTANCE;
 
-document.querySelector<HTMLDivElement>('#video-container')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#video-container")!.innerHTML = `
 <video
   id="video"
   src="${videoSrc}"
@@ -17,28 +17,26 @@ document.querySelector<HTMLDivElement>('#video-container')!.innerHTML = `
 />
 `;
 
+const video = document.getElementById("video") as HTMLVideoElement;
 
-const video = document.getElementById('video') as HTMLVideoElement;
-
-video.addEventListener('loadeddata', () => {
+video.addEventListener("loadeddata", () => {
   const duration = video.duration;
-  console.log('duration', duration);
+  console.log("duration", duration);
 
   video.controls = false;
 
-
   const setVideoTime = (distance: number): void => {
-    const rawTime = Number((distance * duration / TOTAL_DISTANCE).toFixed(6));
-    const time = Math.max(0, Math.min(duration, rawTime))
+    const rawTime = Number(((distance * duration) / TOTAL_DISTANCE).toFixed(6));
+    const time = Math.max(0, Math.min(duration, rawTime));
     console.log(time);
 
     video.currentTime = time;
-  }
+  };
 
   let distance = 3;
 
   setInterval(() => {
-    setVideoTime(distance)
+    setVideoTime(distance);
     distance += 50;
   }, 500);
 });
